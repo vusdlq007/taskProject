68036bdbe9967a6e74edc819ab51aecb0c7cf8bc
vusdlq007 <vusdlq007@naver.com>
2018-08-29 18:12:01 +0900

    수강후기 리스트 통신은 성공 이제 배열받아서 출력하기

---
+++ member/proc/attending_after_select.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018-08-29
+ * Time: 오후 5:03
+ */
---
+++ member/view/lecture_board/attending_after_list.php
@@ -0,0 +1,117 @@
+
+<div id="container" class="container">
+	<?php include "../LNB.php"; ?>
+
+	<div id="content" class="content">
+		<div class="tit-box-h3">
+			<h3 class="tit-h3">수강후기</h3>
+			<div class="sub-depth">
+				<span><i class="icon-home"><span>홈</span></i></span>
+				<span>직무교육 안내</span>
+				<strong>수강후기</strong>
+			</div>
+		</div>
+
+		<ul class="tab-list tab5">
+			<li class="on"><a href="#">전체</a></li>
+			<li><a href="#">일반직무</a></li>
+			<li><a href="#">산업직무</a></li>
+			<li><a href="#">공통역량</a></li>
+			<li><a href="#">어학 및 자격증</a></li>
+		</ul>
+
+		<div class="search-info">
+			<div class="search-form f-r">
+				<select class="input-sel" style="width:158px">
+					<option value="">분류</option>
+				</select>
+				<select class="input-sel" style="width:158px">
+					<option value="">강의명</option>
+					<option value="">작성자</option>
+				</select>
+				<input type="text" class="input-text" placeholder="강의명을 입력하세요." style="width:158px"/>
+				<button type="button" class="btn-s-dark">검색</button>
+			</div>
+		</div>
+
+		<table border="0" cellpadding="0" cellspacing="0" class="tbl-bbs">
+			<caption class="hidden">수강후기</caption>
+			<colgroup>
+				<col style="width:8%"/>
+				<col style="width:8%"/>
+				<col style="*"/>
+				<col style="width:15%"/>
+				<col style="width:12%"/>
+			</colgroup>
+
+			<thead>
+				<tr>
+					<th scope="col">번호</th>
+					<th scope="col">분류</th>
+					<th scope="col">제목</th>
+					<th scope="col">강좌만족도</th>
+					<th scope="col">작성자</th>
+				</tr>
+			</thead>
+	 
+			<tbody>
+				<!-- set -->
+				<tr class="bbs-sbj">
+					<td><span class="txt-icon-line"><em>BEST</em></span></td>
+					<td>CS</td>
+					<td>
+						<a href="#">
+							<span class="tc-gray ellipsis_line">수강 강의명 : Beyond Trouble, 조직을 감동시키는 관계의 기술</span>
+							<strong class="ellipsis_line">절대 후회 없는 강의 예요!</strong>
+						</a>
+					</td>
+					<td>
+						<span class="star-rating">
+							<span class="star-inner" style="width:80%"></span>
+						</span>
+					</td>
+					<td class="last">이름</td>
+				</tr>
+				<!-- //set -->
+				<!-- set -->
+				<tr class="bbs-sbj">
+					<td>1</td>
+					<td>CS</td>
+					<td>
+						<a href="#">
+							<span class="tc-gray ellipsis_line">수강 강의명 : Beyond Trouble, 조직을 감동시키는 관계의 기술</span>
+							<strong class="ellipsis_line">절대 후회 없는 강의 예요!</strong>
+						</a>
+					</td>
+					<td>
+						<span class="star-rating">
+							<span class="star-inner" style="width:80%"></span>
+						</span>
+					</td>
+					<td class="last">이름</td>
+				</tr>
+				<!-- //set -->
+			</tbody>
+		</table>
+
+		<div class="box-paging">
+			<a href="#"><i class="icon-first"><span class="hidden">첫페이지</span></i></a>
+			<a href="#"><i class="icon-prev"><span class="hidden">이전페이지</span></i></a>
+			<a href="#" class="active">1</a>
+			<a href="#">2</a>
+			<a href="#">3</a>
+			<a href="#">4</a>
+			<a href="#">5</a>
+			<a href="#">6</a>
+			<a href="#"><i class="icon-next"><span class="hidden">다음페이지</span></i></a>
+			<a href="#"><i class="icon-last"><span class="hidden">마지막페이지</span></i></a>
+		</div>
+
+		<div class="box-btn t-r">
+			<a href="#" class="btn-m">후기 작성</a>
+		</div>
+	</div>
+</div>
+
+
+
